db470b3f46fea7aa23ddb2ba25d146b03c5cbfaf
oljh <oljh@itu.dk>
2025-01-28 17:31:39 +0100

    finish exercise 00.1

--- exercises/exercise00/main.cpp
+++ exercises/exercise00/main.cpp
@@ -51,26 +51,24 @@ int main()
          0.0f,  0.5f, 0.0f  // top   
     };
 
-    unsigned int VBO, VAO;
     VertexBufferObject vbo;
     VertexArrayObject vao;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    vao.Bind();
+    
+    vbo.Bind();
+    vbo.AllocateData(std::span(vertices, sizeof(vertices) / sizeof(float)));
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    VertexAttribute position(Data::Type::Float, 3);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    vao.SetAttribute(0, position, 0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    vbo.Unbind();
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+    vao.Unbind();
 
 
     // uncomment this call to draw in wireframe polygons.
@@ -88,7 +86,7 @@ int main()
 
         // draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        vao.Bind(); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
 
@@ -100,8 +98,6 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
 
     
--- libraries/itugl/src/ituGL/geometry/VertexArrayObject.cpp
+++ libraries/itugl/src/ituGL/geometry/VertexArrayObject.cpp
@@ -6,28 +6,28 @@
 VertexArrayObject::VertexArrayObject() : Object(NullHandle)
 {
     Handle& handle = GetHandle();
-    // (todo) 00.1: Generate 1 vertex array
+    glGenVertexArrays(1, &handle);
 }
 
 // Get object handle and delete 1 vertex array
 VertexArrayObject::~VertexArrayObject()
 {
     Handle& handle = GetHandle();
-    // (todo) 00.1: Delete 1 vertex array
+    glDeleteVertexArrays(1, &handle);
 }
 
 // Bind the vertex array handle to the specific target
 void VertexArrayObject::Bind() const
 {
     Handle handle = GetHandle();
-    // (todo) 00.1: Bind vertex array
+    glBindVertexArray(handle);
 }
 
 // Bind the null handle to the specific target
 void VertexArrayObject::Unbind()
 {
     Handle handle = NullHandle;
-    // (todo) 00.1: Bind null vertex array
+    glBindVertexArray(0);
 }
 
 // Sets the VertexAttribute pointer and enables the VertexAttribute in that location
@@ -43,6 +43,7 @@ void VertexArrayObject::SetAttribute(GLuint location, const VertexAttribute& att
     pointer += offset;
 
     // (todo) 00.1: Set the VertexAttribute pointer in that location
-
+    glVertexAttribPointer(location, components, type, normalized, attribute.GetSize(), pointer);
     // (todo) 00.1: Finally, we enable the VertexAttribute in that location
+    glEnableVertexAttribArray(0);
 }
