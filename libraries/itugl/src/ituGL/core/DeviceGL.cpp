6cbec9549ac174a3bee68a8ebe804baffd3d4f22
oljh <oljh@itu.dk>
2025-01-28 16:45:42 +0100

    refactor

--- libraries/itugl/src/ituGL/core/DeviceGL.cpp
+++ libraries/itugl/src/ituGL/core/DeviceGL.cpp
@@ -8,15 +8,32 @@ DeviceGL* DeviceGL::m_instance = nullptr;
 
 DeviceGL::DeviceGL() : m_contextLoaded(false)
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 }
 
 DeviceGL::~DeviceGL()
 {
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
 }
 
 // Set the window that OpenGL will use for rendering
 void DeviceGL::SetCurrentWindow(Window& window)
 {
+	GLFWwindow* glfwWindow = window.GetInternalWindow();
+	glfwMakeContextCurrent(glfwWindow);
+	glfwSetFramebufferSizeCallback(glfwWindow, FrameBufferResized);
+	
+	m_contextLoaded = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 }
 
 // Set the dimensions of the viewport
@@ -37,4 +54,7 @@ void DeviceGL::Clear(float r, float g, float b, float a)
 // Callback called when the framebuffer changes size
 void DeviceGL::FrameBufferResized(GLFWwindow* window, GLsizei width, GLsizei height)
 {
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
